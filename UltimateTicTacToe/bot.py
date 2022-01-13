76f09dcdbb9aa8bba815572054a76c2ca78d33f4
Loek Sangers <loek.sangers@hu.nl>
2022-01-13 10:00:35 +0000

    update to working but not performing MCTS ;)

--- UltimateTicTacToe/bot.py
+++ UltimateTicTacToe/bot.py
@@ -4,17 +4,16 @@ from abc import ABC, abstractmethod
 import datetime
 from math import log
 from random import choice, shuffle
-from copy import deepcopy
 
 class MonteCarloNode(ABC):
     """Base class for a Monte Carlo Tree Search state"""
 
-    C = 1.41
+    C = 2
 
     def __init__(self, parent:MonteCarloNode = None) -> None:
         self.visited_count = 0
-        self._wins = 0
-        self._loses = 0
+        self._wins_score = 0
+        self._loses_score = 0
         self.parent = parent
         self.unvisited_actions = []
         self.children = {}
@@ -22,7 +21,7 @@ class MonteCarloNode(ABC):
     @property
     def score(self) -> int:
         """Return the score of this Node."""
-        return self._wins - self._loses
+        return self._wins_score - self._loses_score
 
     @property
     def utc(self) -> float:
@@ -75,20 +74,20 @@ class MonteCarloNode(ABC):
         """
         self.visited_count += 1
         if has_won:
-            self._wins += 1
+            self._wins_score += 1
         else:
-            self._loses += 1
+            self._loses_score += 5
         if self.parent:
             self.parent.backpropagate(not has_won)
 
     def run(self, run_time: float) -> None:
         """Run simulations until we run out of run_time"""
-        iter = 0
+        #iter = 0
         begin = datetime.datetime.now()
         while (datetime.datetime.now() - begin).total_seconds() < run_time:
             self.select_leaf().simulate()
-            iter += 1
-        print(str(iter))
+        #    iter += 1
+        #print(str(iter))
 
 class Action:
     """Class for TicTacToe actions."""
@@ -110,10 +109,15 @@ class Action:
 
 class NormalBoard:
     """Class for normal TicTacToe board."""
-    def __init__(self) -> None:
-        self.local_winner = 0
-        self._grid = [[0 for _ in range(3)] for _ in range(3)]
-        self._moves_count = 0
+    def __init__(self, normal_board: NormalBoard = None) -> None:
+        if normal_board is None:
+            self._grid = [0 for _ in range(9)]
+            self._moves_count = 0
+            self.local_winner = 0
+        else:
+            self._grid = normal_board._grid[:]
+            self._moves_count = normal_board._moves_count
+            self.local_winner = normal_board.local_winner
 
 
     def get_valid_actions(self, current_player:int, base_x:int = 0, base_y:int = 0) -> list[Action]:
@@ -124,7 +128,7 @@ class NormalBoard:
         valid_actions = []
         for y_coord in range(3):
             for x_coord in range(3):
-                if self._grid[y_coord][x_coord] == 0:
+                if self._grid[y_coord*3 + x_coord] == 0:
                     valid_actions.append(Action(base_x + x_coord, base_y + y_coord, current_player))
         return valid_actions
 
@@ -134,10 +138,10 @@ class NormalBoard:
         y_coord = move.y_coord if not localize else move.y_coord % 3
         if x_coord < 0 or x_coord >= 3 \
                 or y_coord < 0 or y_coord >= 3 \
-                or self._grid[y_coord][x_coord] != 0:
+                or self._grid[y_coord*3 + x_coord] != 0:
             raise InvalidMoveError(x_coord, y_coord, self._grid)
 
-        self._grid[y_coord][x_coord] = move.player
+        self._grid[y_coord*3 + x_coord] = move.player
         self._moves_count += 1
 
         return self._update_winner()
@@ -149,29 +153,29 @@ class NormalBoard:
         """
         for index in range(3):
             #rows
-            if self._grid[index][0] > 0 \
-                    and self._grid[index][0] == self._grid[index][1] \
-                    and self._grid[index][0] == self._grid[index][2]:
-                self.local_winner = self._grid[index][0]
+            if self._grid[index*3 + 0] > 0 \
+                    and self._grid[index*3 + 0] == self._grid[index*3 + 1] \
+                    and self._grid[index*3 + 0] == self._grid[index*3 + 2]:
+                self.local_winner = self._grid[index*3 + 0]
                 return self.local_winner
 
             #cols
-            if self._grid[0][index] > 0 \
-                    and self._grid[0][index] == self._grid[1][index] \
-                    and self._grid[0][index] == self._grid[2][index]:
-                self.local_winner = self._grid[0][index]
+            if self._grid[0 + index] > 0 \
+                    and self._grid[0 + index] == self._grid[3 + index] \
+                    and self._grid[0 + index] == self._grid[6 + index]:
+                self.local_winner = self._grid[0 + index]
                 return self.local_winner
 
         #diagonals
-        if self._grid[0][0] > 0 \
-                and self._grid[0][0] == self._grid[1][1] \
-                and self._grid[0][0] == self._grid[2][2]:
-            self.local_winner = self._grid[0][0]
+        if self._grid[0] > 0 \
+                and self._grid[0] == self._grid[4] \
+                and self._grid[0] == self._grid[8]:
+            self.local_winner = self._grid[0]
             return self.local_winner
-        if self._grid[2][0] > 0 \
-                and self._grid[2][0] == self._grid[1][1] \
-                and self._grid[2][0] == self._grid[0][2]:
-            self.local_winner = self._grid[2][0]
+        if self._grid[2] > 0 \
+                and self._grid[2] == self._grid[4] \
+                and self._grid[2] == self._grid[6]:
+            self.local_winner = self._grid[2]
             return self.local_winner
 
         if self._moves_count > 8:
@@ -184,10 +188,10 @@ class UltimateBoard(MonteCarloNode):
     def __init__(self, *, last_move: Action = None, parent: UltimateBoard = None) -> None:
         super().__init__(parent)
         if parent is None:
-            self._grid = [[NormalBoard() for _ in range(3)] for _ in range(3)]
+            self._grid = [NormalBoard() for _ in range(9)]
             self._done_board_count = 0
         else:
-            self._grid = deepcopy(parent._grid)
+            self._grid = [NormalBoard(normal_board) for normal_board in parent._grid]
             self._done_board_count = parent._done_board_count
 
         self.last_move = last_move
@@ -199,7 +203,7 @@ class UltimateBoard(MonteCarloNode):
         if self.last_move is not None:
             x_grid = self.last_move.x_coord % 3
             y_grid = self.last_move.y_coord % 3
-            board = self._grid[y_grid][x_grid]
+            board = self._grid[y_grid*3 + x_grid]
             if board.local_winner == 0:
                 valid_actions = board.get_valid_actions(
                     1 if self.last_move.player != 1 else 2,
@@ -208,16 +212,17 @@ class UltimateBoard(MonteCarloNode):
                 )
 
         if not valid_actions:
-            for y_coord, row in enumerate(self._grid):
-                for x_coord, board in enumerate(row):
-                    if board.local_winner == 0:
-                        valid_actions.extend(
-                            board.get_valid_actions(
-                                1 if self.last_move.player != 1 else 2,
-                                x_coord*3,
-                                y_coord*3
-                            )
+            position: int
+            board: NormalBoard
+            for position, board in enumerate(self._grid):
+                if board.local_winner == 0:
+                    valid_actions.extend(
+                        board.get_valid_actions(
+                            1 if self.last_move.player != 1 else 2,
+                            (position%3)*3,
+                            (position//3)*3
                         )
+                    )
 
         return valid_actions
 
@@ -226,7 +231,7 @@ class UltimateBoard(MonteCarloNode):
         x_grid = move.x_coord // 3
         y_grid = move.y_coord // 3
 
-        winner = self._grid[y_grid][x_grid].play(move, localize = True)
+        winner = self._grid[y_grid*3 + x_grid].play(move, localize = True)
 
         if winner != 0:
             #someone won last board, so check whether game done
@@ -242,29 +247,29 @@ class UltimateBoard(MonteCarloNode):
         """
         for index in range(3):
             #rows
-            if self._grid[index][0].local_winner > 0 \
-                    and self._grid[index][0].local_winner == self._grid[index][1].local_winner \
-                    and self._grid[index][0].local_winner == self._grid[index][2].local_winner:
-                self.local_winner = self._grid[index][0].local_winner
+            if self._grid[index*3 + 0].local_winner > 0 \
+            and self._grid[index*3 + 0].local_winner == self._grid[index*3 + 1].local_winner \
+            and self._grid[index*3 + 0].local_winner == self._grid[index*3 + 2].local_winner:
+                self.local_winner = self._grid[index*3 + 0].local_winner
                 return self.local_winner
 
             #cols
-            if self._grid[0][index].local_winner > 0 \
-                    and self._grid[0][index].local_winner == self._grid[1][index].local_winner \
-                    and self._grid[0][index].local_winner == self._grid[2][index].local_winner:
-                self.local_winner = self._grid[0][index].local_winner
+            if self._grid[0 + index].local_winner > 0 \
+            and self._grid[0 + index].local_winner == self._grid[3 + index].local_winner \
+            and self._grid[0 + index].local_winner == self._grid[6 + index].local_winner:
+                self.local_winner = self._grid[0 + index].local_winner
                 return self.local_winner
 
         #diagonals
-        if self._grid[0][0].local_winner > 0 \
-                and self._grid[0][0].local_winner == self._grid[1][1].local_winner \
-                and self._grid[0][0].local_winner == self._grid[2][2].local_winner:
-            self.local_winner = self._grid[0][0].local_winner
+        if self._grid[0].local_winner > 0 \
+                and self._grid[0].local_winner == self._grid[4].local_winner \
+                and self._grid[0].local_winner == self._grid[8].local_winner:
+            self.local_winner = self._grid[0].local_winner
             return self.local_winner
-        if self._grid[2][0].local_winner > 0 \
-                and self._grid[2][0].local_winner == self._grid[1][1].local_winner \
-                and self._grid[2][0].local_winner == self._grid[0][2].local_winner:
-            self.local_winner = self._grid[2][0].local_winner
+        if self._grid[6 + 0].local_winner > 0 \
+                and self._grid[6 + 0].local_winner == self._grid[4].local_winner \
+                and self._grid[6+ 0].local_winner == self._grid[2].local_winner:
+            self.local_winner = self._grid[6].local_winner
             return self.local_winner
 
         if self._done_board_count > 8:
@@ -290,13 +295,21 @@ class UltimateBoard(MonteCarloNode):
         simulation_board = UltimateBoard(parent=self)
         simulation_board.last_move = self.last_move
         while not simulation_board.is_terminal:
-            simulation_board.play(choice(simulation_board.get_valid_actions()))
+            try:
+                simulation_board.play(choice(simulation_board.get_valid_actions()))
+            except:
+                print(simulation_board.get_valid_actions())
+                for nb in self._grid:
+                    print(nb._grid)
 
-        self.backpropagate(simulation_board.local_winner not in [-1, self.last_move.player])
+        self.backpropagate(simulation_board.local_winner in [self.last_move.player])
 
     def print_move(self):
-        """Print the best move to console."""
-        print(str(self.last_move.x_coord) + " " + str(self.last_move.y_coord))
+        """Print the best move to console.
+        Note: They expect first the y coord then the x coord.
+        """
+        
+        print(str(self.last_move.y_coord) + " " + str(self.last_move.x_coord))
 
 
 # define Python user-defined exceptions
@@ -307,40 +320,43 @@ class InvalidMoveError(Exception):
         self.message = f"Cannot move to {x_coord}, {y_coord} on board {board}"
         super().__init__(self.message)
 
-#first turn
-opponent_row, opponent_col = [int(i) for i in input().split()]
-valid_action_count = int(input())
-for i in range(valid_action_count):
-    move_row, move_col = [int(j) for j in input().split()]
-
-root = UltimateBoard()
-
-if opponent_row == -1:# I start in the middle for no apparent reason
-    root.play(Action(4, 4, 1))
-    root.run(.90)
-    print("4 4")
-else:
-    act = Action(opponent_col, opponent_row, 2)
-    root.play(act)
-    root.run(.90)
-    root = root.best_child
-    root.parent = None
-    root.print_move()
-    for mo in root.children:
-        print(mo)
-
-
-#game loop
-while True:
+def main():
+    """Main"""
+    #first turn
     opponent_row, opponent_col = [int(i) for i in input().split()]
     valid_action_count = int(input())
-    for i in range(valid_action_count):
-        move_row, move_col = [int(j) for j in input().split()]
+    for _ in range(valid_action_count):
+        _, valid_action_count = [int(j) for j in input().split()]
 
-    root = root.children[(2, opponent_col, opponent_row)]
-    root.run(.08)
-    root = root.best_child
-    root.parent = None
-    root.print_move()
-    for mo in root.children:
-        print(mo)
+    root = UltimateBoard()
+
+    if opponent_row == -1:# I start in the middle for no apparent reason
+        root.play(Action(4, 4, 1))
+        root.run(.90)
+        print("4 4")
+    else:
+        act = Action(opponent_col, opponent_row, 2)
+        root.play(act)
+        root.run(.90)
+        root = root.best_child
+        root.parent = None
+        root.print_move()
+
+
+    #game loop
+    while True:
+        opponent_row, opponent_col = [int(i) for i in input().split()]
+        valid_action_count = int(input())
+        for _ in range(valid_action_count):
+            _, _ = [int(j) for j in input().split()]
+
+        if not root.children:
+            root.expand()
+
+        root = root.children[(2, opponent_col, opponent_row)]
+        root.run(.06)
+        root = root.best_child
+        root.parent = None
+        root.print_move()
+
+main()
